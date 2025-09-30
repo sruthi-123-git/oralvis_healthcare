98a4d53b7586ad2a8f39aa58fea549167aa64df5
visualcode <visualcode@gmail.com>
2025-09-30 18:21:57 +0530

    new files

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginForm from '../components/LoginForm';
+
+function LoginPage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>OralVis Healthcare</h1>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default LoginPage;
